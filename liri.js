3b1aa10835a12eae5bce9599118069384e55a97e
Adam Baker <albcodingbootcamp@gmail.com>
2019-11-13 11:28:28 -0500

    added variables and switch statement to liri.js

--- liri.js
+++ liri.js
@@ -1,3 +1,28 @@
 require("dotenv").config();
+
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
+var axios = require("axios");
+
+
+var concertSongMovie = process.argv[2];
+var userSelection = process.argv[3];
+
+switch (concertSongMovie) {
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    
+    case "movie-this":
+        movieThis();
+        break;
+        
+    case "do-what-it-says":
+        doWhatitSays();
+        break;
+}
